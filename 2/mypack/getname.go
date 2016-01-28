94962afdca10c708c40328989d9a8eebcd688dd1
Sergey Melekhin <sergey@melekhin.me>
2016-01-28 18:57:11 +1000

    Added two examples

---
+++ 2/mypack/getname.go
@@ -0,0 +1,6 @@
+package mypack
+
+func GetName() string {
+	name := "Мумий Тролль"
+	return name
+}
---
+++ 2/run.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/c-pro/go-demo/2/mypack"
+)
+
+func main() {
+	fmt.Printf("Hello, %s!\n", mypack.GetName())
+}
